15223b02c242c499e120dc464bfa97aa5e3965ea
petergodot7 <152039813+petergodot7@users.noreply.github.com>
2024-09-24 15:52:56 -0400

    Milestone 2. Home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,25 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using Assignment1.Models;
-
-namespace Assignment1.Controllers;
-
-public class HomeController : Controller
-{
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+namespace PartyInvites.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm() {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) {
+            if (ModelState.IsValid) {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            } else {
+                return View();
+            }
+        }
+        public ViewResult ListResponses() {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
     }
 }
